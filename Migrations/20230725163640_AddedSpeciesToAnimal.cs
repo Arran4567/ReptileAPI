0d70fa932872f0be438cdde78a64f8db904e2fdc
matjones95 <139883199+matjones95@users.noreply.github.com>
2023-07-25 17:44:49 +0100

    Added species to animals (#5)

---
+++ Migrations/20230725163640_AddedSpeciesToAnimal.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReptileAPI.Migrations
+{
+    public partial class AddedSpeciesToAnimal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Species",
+                table: "Animals",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Species",
+                table: "Animals");
+        }
+    }
+}
