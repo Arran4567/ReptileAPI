bcff04440a9cc82b882c874580a9a71b0daf42d6
Mathew Jones <mjmoither@gmail.com>
2023-07-28 19:32:46 +0100

    Added Display Names

--- Models/Animal.cs
+++ Models/Animal.cs
@@ -10,12 +10,28 @@ namespace ReptileAPI.Models
 
         public string Name { get; set; }
 
+        public string Species { get; set; }
+
+        public string Sex { get; set; }
+
         [DisplayName("Date of Birth")]
         public DateTime DOB { get; set; }
 
         [NotMapped]
-        public int Age { get{ return new DateTime((DateTime.Now - DOB).Ticks).Year - 1;} }
+        public int Age { get { return new DateTime((DateTime.Now - DOB).Ticks).Year - 1; } }
 
-        public string? Species { get; set; } 
+        public string? Morphs { get; set; }
+
+        [DisplayName("Feeding Size")]
+        public string? Feeding_Size { get; set; }
+
+        [DisplayName("Feeding Schedule")]
+        public int? Feeding_Schedule { get; set; }
+
+        [DisplayName("Water Change")]
+        public int? Water_Change { get; set; }
+
+        [DisplayName("Bedding Change")]
+        public int? Bedding_Change { get; set; }
     }
 }
